777dbb6a9a5b8f3b7593487128996e91b12dc2a0
Luis M. Barquillo <lmbarquillo@gmail.com>
2019-05-25 11:03:00 +0200

    Añadidos archivos faltantes

---
+++ app/src/main/java/munoz/alejandro/agendaalejandrom/ContactosAdapter.java
@@ -0,0 +1,55 @@
+package munoz.alejandro.agendaalejandrom;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ContactosAdapter extends BaseAdapter {
+    private ArrayList<Contacto> misContactos;
+    private Context context;
+
+    public ContactosAdapter(ArrayList<Contacto> misContactos, Context context) {
+        this.misContactos = misContactos;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return misContactos.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return misContactos.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View v=view;
+        if(v==null){
+            LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v=inflater.inflate(R.layout.contactoslayout,null);
+
+            ImageView foto=v.findViewById(R.id.imgContacto);
+            TextView nombre=v.findViewById(R.id.txtNombre);
+            TextView telefono=v.findViewById(R.id.txtTelefono);
+            TextView aviso=v.findViewById(R.id.txtMensaje);
+
+            nombre.setText(misContactos.get(i).getNombre());
+            telefono.setText(misContactos.get(i).getTelefono()  );
+        }
+        return v;
+    }
+}
---
+++ app/src/main/java/munoz/alejandro/agendaalejandrom/EditarContactoActivity.java
@@ -0,0 +1,13 @@
+package munoz.alejandro.agendaalejandrom;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditarContactoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editar_contacto);
+    }
+}
